61ee387944aa885b9da658971ff3dd104d11a7ca
Adarsh Desai <desaiadarsh22@gmail.com>
2023-01-06 19:19:01 +0530

    Moved timeout function from controller.js to helper.js

--- src/js/controller.js
+++ src/js/controller.js
@@ -3,14 +3,6 @@ import recipeView from './view/recipeView';
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
-const timeout = function (s) {
-  return new Promise(function (_, reject) {
-    setTimeout(function () {
-      reject(new Error(`Request took too long! Timeout after ${s} second`));
-    }, s * 1000);
-  });
-};
-
 const controlRecipes = async function () {
   try {
     // Get the hash of the recipe
--- src/js/helper.js
+++ src/js/helper.js
@@ -1,6 +1,19 @@
+import { TIMEOUT } from './config';
+
+const timeout = function (s) {
+  return new Promise(function (_, reject) {
+    setTimeout(function () {
+      reject(new Error(`Request took too long! Timeout after ${s} second`));
+    }, s * 1000);
+  });
+};
+
 export const getJSON = async function (url) {
   try {
-    const response = await fetch(url);
+    // Stop the page from loading forever by using Promise.race function.
+    // Whichever promise gets resolved first will win
+    const fetchData = fetch(url);
+    const response = await Promise.race([fetchData, timeout(TIMEOUT)]);
     const data = await response.json();
     // If something goes wrong with the request
     if (!response.ok) throw new Error(`${data.message} ${response.status}`);
