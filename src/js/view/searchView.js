b6145d1e68618a781acea605c6809497d0ec00df
Adarsh Desai <desaiadarsh22@gmail.com>
2023-01-07 12:14:46 +0530

    Created searchView class to fetch query and handle events in the search bar

--- src/js/view/searchView.js
+++ src/js/view/searchView.js
@@ -1,1 +1,20 @@
+class SearchView {
+  #parentElement = document.querySelector('.search');
 
+  getQuery() {
+    return this.#parentElement.querySelector('.search__field').value;
+  }
+
+  addHandlerSearch(handler) {
+    this.#parentElement.addEventListener('submit', e => {
+      e.preventDefault(); //Default form behaviour: Refresh on submit
+      handler();
+    });
+  }
+
+  clearInput() {
+    this.#parentElement.querySelector('.search__field').value = '';
+  }
+}
+
+export default new SearchView();
