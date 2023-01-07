87aaf1c5e0f26d09d18ae16cdaa62d9294036bd8
Adarsh Desai <desaiadarsh22@gmail.com>
2023-01-07 13:21:12 +0530

    Removed class of active on side bar list elements

--- src/js/view/resultsView.js
+++ src/js/view/resultsView.js
@@ -9,7 +9,7 @@ class ResultsView extends View {
 
   _generateMarkupPreview(result) {
     return `<li class="preview">
-        <a class="preview__link preview__link--active" href="#${result.id}">
+        <a class="preview__link" href="#${result.id}">
           <figure class="preview__fig">
             <img src="${result.image}" alt="${result.title}" />
           </figure>
